d8092c7e814742fb3684d4758f511cceae082f6e
コバヤシタカヒト <k022g0105@g.neec.ac.jp>
2023-05-16 17:46:44 +0900

    j

--- EnemyBullet.cpp
+++ EnemyBullet.cpp
@@ -1,1 +1,38 @@
 ﻿#include "EnemyBullet.h"
+#include "PlayerBullet.h"
+#include <assert.h>
+#include "Vector3.h"
+#include "world.h"
+
+
+void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector3& velocity) {
+	// NULLポインタチェック
+	assert(model);
+	model_ = model;
+
+	// テクスチャ読み込む
+	texturehandle_ = TextureManager::Load("block.png");
+
+	// ワールドトランスフォーの初期化
+	worldTransform_.Initialize();
+	worldTransform_.translation_ = Vec3Add(worldTransform_.translation_, position);
+	worldTransform_.UpdateMatrix();
+
+	velocity_ = velocity;
+}
+void EnemyBullet::Update() {
+	worldTransform_.UpdateMatrix();
+	worldTransform_.translation_ = Vec3Add(worldTransform_.translation_, velocity_);
+	worldTransform_.matWorld_ = MakeAffineMatrix(
+	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
+
+	// 時間経過でデス
+	if (--dethTimer_ <= 0) {
+		isDead_ = true;
+	}
+}
+
+void EnemyBullet::Draw(const ViewProjection& viewProjection) {
+
+	model_->Draw(worldTransform_, viewProjection, texturehandle_);
+}
--- EnemyBullet.h
+++ EnemyBullet.h
@@ -1,4 +1,34 @@
 ﻿#pragma once
-class EnemyBullet {
+#include "Model.h"
+#include "WorldTransform.h"
+#include "input.h"
+#include "Vector3.h"
 
+class EnemyBullet {
+public:
+	/// 初期化
+	void Initialize(Model* model, const Vector3& position, const Vector3& velocity);
+
+	void Update();
+
+	void Draw(const ViewProjection& viewProjection);
+
+	bool IsDead() const { return isDead_; }
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// テクスチャハンドル
+	uint32_t texturehandle_ = 0u;
+
+	Vector3 velocity_;
+
+	// 寿命
+	static const int32_t kLifeTime = 60 * 5;
+	// デスタイマー
+	int32_t dethTimer_ = kLifeTime;
+	// デスフラグ
+	bool isDead_ = false;
 };
